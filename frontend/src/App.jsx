922ef3ddc2c244146c505d63abaee44aa73fff7f
krishna976762 <krishnabokefod@gmail.com>
2024-10-29 16:24:24 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
+
+function App() {
+  return (
+    <div data-theme="light">
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
